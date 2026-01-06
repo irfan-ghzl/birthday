1a60c9b4a81cde309c3914c188269a834aa539fc
irfan-ghzl <aigirfan.g@gmail.com>
2026-01-06 23:27:28 +0700

    extended cd

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,20 +1,20 @@
 // ==================== CONFIGURATION ====================
 // Ganti tanggal ulang tahun di sini (format: tahun, bulan-1, tanggal, jam, menit, detik)
 // Contoh: 1 Februari 2026 jam 00:00:00 = new Date(2026, 1, 1, 0, 0, 0)
-const BIRTHDAY_DATE = new Date(2026, 0, 7, 0, 0, 0); // 7 Januari 2026
+const BIRTHDAY_DATE = new Date(2026, 0, 7, 3, 0, 0); // 7 Januari 2026 jam 03:00:00
 
 // Nama orang yang berulang tahun
 const BIRTHDAY_PERSON = "SISTI AGIEL YULAIKA";
 
 // Password untuk membuka hadiah (setelah countdown selesai)
 // Ganti password ini sesuai keinginanmu
-const SECRET_PASSWORD = "iloveyou";
+const SECRET_PASSWORD = "si lesung";
 
 // URL halaman-halaman
 const PAGES = {
-    countdown: '../../pages/countdown/index.html',
-    celebration: '../../pages/celebration/index.html',
-    gifts: '../../pages/gifts/index.html'
+    countdown: '../../pages/countdown/countdown.html',
+    celebration: '../../pages/celebration/celebration.html',
+    gifts: '../../pages/gifts/gifts.html'
 };
 
 // Console Easter Egg
